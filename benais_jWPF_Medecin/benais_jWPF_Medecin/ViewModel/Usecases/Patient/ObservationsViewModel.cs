ae3f0a19137e1968aa97b7fb3f5954f401ae1256
Mickael AU <mickael.au@gmail.com>
2018-12-01 01:37:46 +0100

    [OBSERVATIONS][DISPLAY] Changed UI

--- benais_jWPF_Medecin/benais_jWPF_Medecin/ViewModel/Usecases/Patient/ObservationsViewModel.cs
+++ benais_jWPF_Medecin/benais_jWPF_Medecin/ViewModel/Usecases/Patient/ObservationsViewModel.cs
@@ -14,23 +14,27 @@ using System.Windows.Controls;
 
 namespace benais_jWPF_Medecin.ViewModel.Usecases.Patient
 {
-    public class ObservationsViewModel: BaseViewModel
+    public class ObservationsViewModel : BaseViewModel
     {
         #region Variables
 
         private string _currentLogin;
         private int _idPatient;
         private bool _isLoading;
+        private bool _showObservation;
         private bool _isAddView;
         private PatientBM _patientBM;
         private ObservationBM _observationBM;
         private ServicePatientReference.Patient _selectedPatient;
         private ServicePatientReference.Observation _selectedObservation;
-        private ObservableCollection<ServicePatientReference.Observation> _observationList;
 
+        private ObservableCollection<ServicePatientReference.Observation> _observationList;
         private ObservableCollection<string> _datesCollection;
+
         private SeriesCollection _bloodPressureCollection;
+        private bool _showBloodPressureGraph;
         private SeriesCollection _weightCollection;
+        private bool _showWeightGraph;
 
         private UserControl _addObservationUC;
 
@@ -56,6 +60,18 @@ namespace benais_jWPF_Medecin.ViewModel.Usecases.Patient
                 OnPropertyChanged(nameof(IsAddView));
             }
         }
+        public bool ShowObservation
+        {
+            get { return _showObservation; }
+            set
+            {
+                if (!IsAddView)
+                {
+                    _showObservation = value;
+                    OnPropertyChanged(nameof(ShowObservation));
+                }
+            }
+        }
         public ServicePatientReference.Patient SelectedPatient
         {
             get { return _selectedPatient; }
@@ -71,6 +87,7 @@ namespace benais_jWPF_Medecin.ViewModel.Usecases.Patient
             set
             {
                 _selectedObservation = value;
+                ShowObservation = (value == null) ? false : true;
                 OnPropertyChanged(nameof(SelectedObservation));
             }
         }
@@ -92,6 +109,7 @@ namespace benais_jWPF_Medecin.ViewModel.Usecases.Patient
                 OnPropertyChanged(nameof(DatesCollection));
             }
         }
+
         public SeriesCollection BloodPressureCollection
         {
             get { return _bloodPressureCollection; }
@@ -101,6 +119,15 @@ namespace benais_jWPF_Medecin.ViewModel.Usecases.Patient
                 OnPropertyChanged(nameof(BloodPressureCollection));
             }
         }
+        public bool ShowBloodPressureGraph
+        {
+            get { return _showBloodPressureGraph; }
+            set
+            {
+                _showBloodPressureGraph = value;
+                OnPropertyChanged(nameof(ShowBloodPressureGraph));
+            }
+        }
         public SeriesCollection WeightCollection
         {
             get { return _weightCollection; }
@@ -108,6 +135,15 @@ namespace benais_jWPF_Medecin.ViewModel.Usecases.Patient
             {
                 _weightCollection = value;
                 OnPropertyChanged(nameof(WeightCollection));
+            }
+        }
+        public bool ShowWeightGraph
+        {
+            get { return _showWeightGraph; }
+            set
+            {
+                _showWeightGraph = value;
+                OnPropertyChanged(nameof(ShowWeightGraph));
             }
         }
 
@@ -120,7 +156,7 @@ namespace benais_jWPF_Medecin.ViewModel.Usecases.Patient
                 OnPropertyChanged(nameof(AddObservationUC));
             }
         }
-        
+
         #endregion
 
         #region Constructor
@@ -132,6 +168,7 @@ namespace benais_jWPF_Medecin.ViewModel.Usecases.Patient
             _patientBM = new PatientBM();
             _observationBM = new ObservationBM();
             ObservationsList = new ObservableCollection<ServicePatientReference.Observation>();
+            ShowObservation = false;
             IsAddView = false;
             InitializeCommands();
             InitializeGraph();
@@ -157,6 +194,7 @@ namespace benais_jWPF_Medecin.ViewModel.Usecases.Patient
 
         public void AddObservation()
         {
+            SelectedObservation = null;
             IsAddView = true;
             AddObservationUC = new AddObservationUC();
         }
@@ -209,7 +247,8 @@ namespace benais_jWPF_Medecin.ViewModel.Usecases.Patient
                         pressureList.Add(observation.BloodPressure);
                         dateList.Add(observation.Date.ToString());
                     }
-                    DispatchService.Invoke(() => {
+                    DispatchService.Invoke(() =>
+                    {
                         ObservationsList = list;
                         UpdateGraph(weightList, pressureList, dateList);
                     });
@@ -250,7 +289,9 @@ namespace benais_jWPF_Medecin.ViewModel.Usecases.Patient
         private void UpdateGraph(List<int> weightList, List<int> pressureList, List<string> dateList)
         {
             BloodPressureCollection[0].Values = weightList.AsChartValues();
+            ShowBloodPressureGraph = weightList.Count != 0;
             WeightCollection[0].Values = pressureList.AsChartValues();
+            ShowWeightGraph = pressureList.Count != 0;
             DatesCollection = new ObservableCollection<string>(dateList);
         }
 
@@ -260,6 +301,7 @@ namespace benais_jWPF_Medecin.ViewModel.Usecases.Patient
             {
                 ServiceObservationReference.Observation observation = (ServiceObservationReference.Observation)param;
                 _observationBM.AddObservation(_idPatient, observation);
+                IsAddView = false;
                 InitializePatient(_idPatient);
             }
             catch (Exception)
